d50ca8a081283864cb9d2217345ecd678a57dd56
EricYangXD <ericyangxinde@gmail.com>
2018-04-09 15:52:52 +0800

    add 2 new files

--- src/webworkers/worker.js
+++ src/webworkers/worker.js
@@ -1,1 +1,18 @@
-console.log("23333333");
+addEventListener("message", messageHandler, true);
+
+var onmessage = function(e) {
+    var d = e.data; //获取发送来的数据
+    postMessage(d); //将获取到的数据发送回主线程
+    console.log("23333333");
+};
+
+
+worker.addEventListener("error", errorHandler, true);
+
+function errorHandler(e) {
+    console.log(e.message, e);
+}
+
+function messageHandler(e) {
+    postMessage("worker says:" + e.data + "too");
+}
