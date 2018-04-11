47909ca9587a8a747eb225805417fc6a38229119
EricYangXD <ericyangxinde@gmail.com>
2018-04-11 10:31:14 +0800

    add simulated annealing

---
+++ src/webworkers/fibonacci.js
@@ -0,0 +1,8 @@
+var fibonacci = function(n) {
+    return n < 2 ? n : arguments.callee(n - 1) + arguments.callee(n - 2);
+};
+
+onmessage = function(e) {
+    var n = parseInt(e.data, 10);
+    postMessage(fibonacci(n));
+};
