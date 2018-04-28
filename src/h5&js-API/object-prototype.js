bd8e39ba69941b7ac846716cdc5447de53ed11e2
EricYangXD <ericyangxinde@gmail.com>
2018-04-28 14:08:05 +0800

    add 1

--- src/h5&js-API/object-prototype.js
+++ src/h5&js-API/object-prototype.js
@@ -5,3 +5,46 @@ try {
 } finally {
     console.log('finally');
 }
+var obj = {};
+obj.attribute = 'trash';
+console.log(obj.__proto__);
+console.log(Object.prototype);
+console.log(obj.__proto__ === Object.prototype);
+
+function Dog(name) {
+    this.name = name;
+}
+
+Dog.prototype.callName = function() {
+    console.log(this.name, 'wangwang');
+}
+
+let dog = new Dog("Three SBs");
+dog.printName = function() {
+    console.log(this.name);
+}
+dog.callName();
+dog.printName();
+
+console.log(s);
+var s = 1;
+
+var b;
+console.log(b);
+b = 2;
+
+var a = {
+    name: 'A',
+    fn: function() {
+        console.log(this.name);
+    }
+}
+a.fn(); //A
+
+a.fn.call({ name: 'B' }); //B
+
+var fn1 = a.fn;
+fn1(); //undefined
+
+var name = "C";
+fn1(); //C
